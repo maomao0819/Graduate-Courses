779c80552ced64885f8371f50580189131789a15
maomao0819 <maomao.08190@gmail.com>
2022-11-04 23:30:37 +0800

    Add files via upload

---
+++ Applied Deep Learning/ADL21-HW2/run_trainer.sh
@@ -0,0 +1,31 @@
+# Copyright 2020 The HuggingFace Team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+python run_swag.py \
+  --do_train \
+  --do_eval \
+  --model_name_or_path bert-base-chinese \
+  --output_dir /tmp/test-swag-trainer \
+  --pad_to_max_length \
+  --train_file train.json \
+  --validation_file valid.json \
+  --context_file context.json \
+  --preprocessing_num_workers 8 \
+  --cache_dir ./cache/ \
+  --context_file context.json \
+  --max_seq_length 512 \
+  --gradient_accumulation_steps 2 \
+  --data_seed 888 \
+  --auto_find_batch_size True \
+  --warmup_ratio 0.1
