91aac82ab6d06338f1357159c3f290a6ca56c21f
maomao0819 <maomao.08190@gmail.com>
2022-11-10 21:14:25 +0800

    modify qa

--- Applied Deep Learning/HW2/question_answering/run_qa.py
+++ Applied Deep Learning/HW2/question_answering/run_qa.py
@@ -631,7 +631,7 @@ def main():
         references = [{"id": ex["id"], "answer": ex[answer_column_name]} for ex in examples]
         return EvalPrediction(predictions=formatted_predictions, label_ids=references)
 
-    metric = evaluate.load("squad_v2" if data_args.version_2_with_negative else "squad")
+    # metric = evaluate.load("squad_v2" if data_args.version_2_with_negative else "squad")
 
     def compute_metrics(p: EvalPrediction):
         return compute_score.compute(predictions=p.predictions, references=p.label_ids)
